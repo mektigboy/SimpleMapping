2abb182a0aa6a16a303142e6ce1f16ff0fbceeaa
mektigboy <mektigboy@protonmail.com>
2022-07-28 14:22:01 -0500

    Update Solidity version

--- SimpleMapping.sol
+++ SimpleMapping.sol
@@ -2,14 +2,14 @@
 pragma solidity ^ 0.8.15;
 
 contract SimpleMapping {
-  mapping(uint256 => bool) public myMapping;
-  mapping(address => bool) public myAddressMapping;
+    mapping(uint256 => bool) public myMapping;
+    mapping(address => bool) public myAddressMapping;
 
-  function setValue(uint256 _index) public {
-    myMapping[_index] = true;
-  }
+    function setValue(uint256 _index) public {
+        myMapping[_index] = true;
+    }
 
-  function setMyAddressToTrue() public {
-    myAddressMapping[msg.sender] = true;
-  }
+    function setMyAddressToTrue() public {
+        myAddressMapping[msg.sender] = true;
+    }
 }
